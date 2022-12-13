ae0a362168325c8cf2f1b18426549af9bfbe8454
Corey Sawin <sawincp@gmail.com>
2022-12-13 16:22:01 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset()
+  })
+})
+
+function buildToDo(todo){
+  console.log(todo)
+  let p = document.createElement('p');
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${todo}  `;
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
